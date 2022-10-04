bc23945bd63decc7b2f80c1eee2e993ee94d0368
Kurt Seifried <kurt@seifried.org>
2022-10-03 22:52:20 -0600

    schemas

--- GSDfiletools/GSDLoadConfigs.py
+++ GSDfiletools/GSDLoadConfigs.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-class GSDLoadConfigs:
+class LoadConfigs:
     def __init__(self, testing=False):
         # Set gsdconfig globals like pathnames
         # TODO: check for trailing slash at some point
@@ -33,3 +33,24 @@ class GSDLoadConfigs:
 
     def get_gsd_tools_path(self):
         return(self.gsd_tools_path)
+
+class LoadJSONFile:
+    def __init__(self, file):
+        self.file = file
+        self.file_path, self.file_name = os.path.split(os.path.realpath(self.file))
+    def get_file_name(self):
+        return(self.file_name)
+    def get_file_path(self):
+        return(self.file_path)
+    def get_JSON_content(self):
+        with open(self.file, 'r') as f:
+            try:
+                self.JSON_content = json.load(f)
+            except ValueError as e:
+                print("ERROR: " + self.file_path + "/" + self.file_name + " is not a valid JSON file")
+                quit()
+        return(self.JSON_content)
+
+class IdentifyJSONFileType:
+    def __init__(self, file_name, file_content):
+        print("odoo")
--- testing.py
+++ testing.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 
+import os
+import sys
 import GSDfiletools
 
-gsd_config = GSDfiletools.GSDLoadConfigs()
+###################
+#
+# Standard stuff
+#
+###################
 
-print(gsd_config.get_gsd_database_path())
-print(gsd_config.get_gsd_tools_path())
+
+if __name__ == "__main__":
+    
+    gsd_config = GSDfiletools.LoadConfigs()
+    
+    print("gsd_database_path: " + gsd_config.get_gsd_database_path())
+    print("gsd_database_path: " + gsd_config.get_gsd_tools_path())
+
+    filename =  sys.argv[1]
+
+    file_path, file_name = os.path.split(os.path.realpath(sys.argv[0]))
+    print("file_name: " + file_path)
+
+    gsd_file = GSDfiletools.LoadJSONFile(filename)
+    gsd_file_content = gsd_file.get_JSON_content()
+    print("full_path: " + gsd_file.get_file_path())
+
+    print(gsd_file_content)
